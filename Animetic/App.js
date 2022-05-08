6561402f2c1fc72e1c1362fb7461a22b3131b99e
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-08 21:29:55 +0300

    Add files via upload

--- Animetic/App.js
+++ Animetic/App.js
@@ -5,4 +5,6 @@ bridge.subscribe((e) => console.log("vkBridge event", e));
 bridge.send("VKWebAppInit", {});
 
 //поделиться
+function share1(){
 bridge.send("VKWebAppShare", {"link": "vk.com/app8156273#Anime_games"});
+}
