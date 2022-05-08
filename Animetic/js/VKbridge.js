18b80637171a0f86786f250cbb127d5bb235ef84
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-08 19:11:15 +0300

    Delete VKbridge.js

--- Animetic/js/VKbridge.js
+++
@@ -1,7 +0,0 @@
-import bridge from '@vkontakte/vk-bridge';
-
-// Sends event to client
-bridge.send('VKWebAppInit');
-
-// Subscribes to event, sended by client
-bridge.subscribe(e => console.log(e));
