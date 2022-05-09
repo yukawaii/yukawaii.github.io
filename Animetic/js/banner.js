a63eed81c9a387df1923e18e11a7c1acb9683f5d
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-09 18:44:01 +0300

    Add files via upload

---
+++ Animetic/js/banner.js
@@ -0,0 +1,18 @@
+import "@vkontakte/vkui/dist/vkui.css";
+import { PromoBanner } from '@vkontakte/vkui';
+
+const Banner = () => {
+    const [bannerData, setBannerData] = useState({});
+  
+    useEffect(() => {
+      bridge.send("VKWebAppGetAds").then((bannerInfo) => {
+        setBannerData(bannerInfo);
+      });
+    }, []);
+  
+    if (!bannerData) {
+      return null;
+    }
+  
+    return <PromoBanner bannerData={bannerData} />;
+  }
