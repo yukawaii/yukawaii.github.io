6176a470790674404ace4a035a90eb08772e6403
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-12 19:56:50 +0300

    Add files via upload

---
+++ hirr/App.js
@@ -0,0 +1,18 @@
+//инициализация
+const bridge = vkBridge.default;
+bridge.subscribe((e) => console.log("vkBridge event", e));
+bridge.send("VKWebAppInit", {});
+
+//поделиться
+function share1(){
+bridge.send("VKWebAppShare", {"text": "Интересные игры!", "link": "vk.com/app8156273"});
+}
+function favor1(){
+bridge.send("VKWebAppAddToFavorites");
+}
+function myadd1(){
+  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+  bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+.then(data => console.log(data.result))
+.catch(error => console.log(error));
+}
