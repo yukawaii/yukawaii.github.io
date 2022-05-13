a917f8e2e313d9fbcbd0d7cf0e88723910077c7b
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-13 15:21:26 +0300

    Add files via upload

---
+++ AniM/App.js
@@ -0,0 +1,23 @@
+//инициализация
+const bridge = vkBridge.default;
+bridge.subscribe((e) => console.log("vkBridge event", e));
+bridge.send("VKWebAppInit", {});
+
+//поделиться
+
+function share1(){
+  bridge.send("VKWebAppShowWallPostBox", {
+    "message": "Аниме-пазлы на каждый день!",
+    "attachments": "https://vk.com/animepuzz"
+  });
+}
+function favor1(){
+bridge.send("VKWebAppAddToFavorites");
+}
+function myadd1(){
+  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+  bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+.then(data => console.log(data.result))
+.catch(error => console.log(error));
+}
+
