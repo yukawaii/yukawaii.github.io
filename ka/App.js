b29dfd1c120bb40420ed9600be7fd2842173e7d0
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-20 13:08:03 +0300

    Add files via upload

--- ka/App.js
+++ ka/App.js
@@ -18,10 +18,9 @@ function favor1(){
 bridge.send("VKWebAppAddToFavorites");
 }
 
-/*function myadd1(){
+function myadd1(){
   bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
   bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 .then(data => console.log(data.result))
 .catch(error => console.log(error));
 }
-*/
