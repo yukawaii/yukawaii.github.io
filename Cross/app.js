3463481a7261d259cb8a4ea7d7abde70d6384ff7
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-24 18:48:24 +0300

    Add files via upload

---
+++ Cross/app.js
@@ -0,0 +1,34 @@
+//инициализация
+const bridge = vkBridge.default;
+bridge.subscribe((e) => console.log("vkBridge event", e));
+bridge.send("VKWebAppInit", {});
+
+//поделиться
+function share1(){
+bridge.send("VKWebAppShare", {"text": "Интересные игры!", "link": "vk.com/app8156273"});
+}
+function share2(){
+  bridge.send("VKWebAppShowWallPostBox", {
+    "message": "Игры для быстрого запоминания хираганы!",
+    "attachments": "https://vk.com/app8165024"
+  });
+}
+
+function favor1(){
+bridge.send("VKWebAppAddToFavorites");
+}
+function myadd1(){
+  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+  bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+.then(data => console.log(data.result))
+.catch(error => console.log(error));
+}
+function joingroup(){
+  bridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+}
+function myadd2(){
+  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "reward"});
+  bridge.send("VKWebAppShowNativeAds", {ad_format:"reward"})
+.then(data => console.log(data.result))
+.catch(error => console.log(error));
+}
