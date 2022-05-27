a71a3d4dbffaa47be1cd72eea110922c739702fa
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-27 23:52:44 +0300

    Add files via upload

--- Cross/App.js
+++ Cross/App.js
@@ -35,15 +35,25 @@ function infr(){
   bridge.send("VKWebAppShowInviteBox", {})
 }
 //турнирная табличка
-function ressend(){
-  bridge.send("secure.addAppEvent", {
-      activity_id: 2,
-      value: score1
-  })}
-
   function top1(){
-    bridge.send("secure.addAppEvent", {activity_id: 2, value: score1, global:1});
+    VK.Api.call("secure.addAppEvent",{activity_id: 2, value: score1, global:1,  v:"5.73"});
+    var score2;
+    VK.Api.call("apps.getScore",{}, function(r) {
+      if(r.response) {
+      score2=r.response[0]; }
+      });;
+    bridge.send("VKWebAppShowLeaderBoardBox", {user_result: score2, global:1})
+    .then(data => console.log(data.success))  
+   .catch(error => console.log(error));
+    } 
+
+    //past try
+    /*
+    
+  function top1(){
+    VK.Api.call("secure.addAppEvent",{activity_id: 2, value: score1, global:1,  v:"5.73"});
     bridge.send("VKWebAppShowLeaderBoardBox", {user_result: score1, global:1})
     .then(data => console.log(data.success))  
    .catch(error => console.log(error));
     } 
+    */
