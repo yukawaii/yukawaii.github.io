e508174ac49388b2baf773104c009e53f4c2a4aa
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-05-28 13:53:35 +0300

    Add files via upload

--- Cross/index.js
+++ Cross/index.js
@@ -1110,7 +1110,7 @@ function getinfo1(){
     .catch(error => console.log(error));
     
     //получение очков из вк
-    bridge.send("VKWebAppCallAPIMethod", {"method": "apps.getScore", "request_id": "getscore", "params": {"user_id": userid, "v":"5.9", "access_token":token, global:1}})
+    bridge.send("VKWebAppCallAPIMethod", {"method": "apps.getScore", "request_id": "getscore", "params": {"user_id": userid, "v":"5.131", "access_token":token, global:1}})
     .then(response => {console.log("Очков на вк:" + response);
       // *назначение переменных*
       score2= response;
@@ -1120,7 +1120,7 @@ function getinfo1(){
 //send score
 function sendscore1(){
     getinfo1();
-    bridge.send("VKWebAppCallAPIMethod", {"method": "secure.addappEvent", "request_id": "appevent_from_script", "params": {"client_secret":"qp47UOdcqJmW94rKknxR","user_id": userid, "activity_id":2, "value": score1, "v": "5.9", "access_token": token}})
+    bridge.send("VKWebAppCallAPIMethod", {"method": "secure.addappEvent", "request_id": "appevent_from_script", "params": {"client_secret":"qp47UOdcqJmW94rKknxR","user_id": userid, "activity_id":2, "value": score1, "v": "5.131", "access_token": token}})
     .then(response => {console.log("Ответ на добавление очков:" + response);
     })
     .catch(error => console.log(error)); }
