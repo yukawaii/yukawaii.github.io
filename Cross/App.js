ceb392f907cf3a5666462ff375f1f14d8de52ce2
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-06-02 17:28:06 +0300

    Add files via upload

--- Cross/App.js
+++ Cross/App.js
@@ -32,9 +32,9 @@ function infr(){
   bridge.send("VKWebAppShowInviteBox", {})
 }
 //турнирная табличка
-var score1;
+var score1, userid, score2;
 
-function ressend(){
+function ressend1(){
   score1=this.maxLane;
   bridge.send("secure.addAppEvent", {
       activity_id: 2,
@@ -45,3 +45,39 @@ function ressend(){
       .then(data => console.log(data.success))  
      .catch(error => console.log(error));
       } 
+      /////////////////////////////////////////
+      function getinfo(){
+        bridge.send('VKWebAppGetUserInfo')
+        .then(data => {console.log(data.id);
+            // *назначение переменных*
+        userid = data.id;
+        })
+        .catch(error => console.log(error));
+     /*   
+        bridge.send("VKWebAppGetAuthToken", {"app_id": 8171561, "scope": "friends,status"})
+        .then(data => {console.log(data.access_token);
+            // *назначение переменных*
+        token = data.access_token;
+        })
+        .catch(error => console.log(error));   */
+        
+        //отправка очков в вк
+        function ressend(){
+          score1=this.maxLane;
+        bridge.send("VKWebAppCallAPIMethod", {"method": "secure.addappEvent", "request_id": "32test", "params":
+          {"user_id":userid, 
+          "activity_id":2, "value":score1,
+           "v": "5.1", "access_token":a79a560da79a560da79a560d9da7e6e624aa79aa79a560dc51cd511726b4813a807b9ec}})
+        .then(response => {console.log("Ответ на добавление очков:" + response);
+        })
+        .catch(error => console.log(error)); }
+        
+        //получение очков из вк
+        bridge.send("VKWebAppCallAPIMethod", {"method": "apps.getScore", "request_id": "32test", "params": {"user_id": {userid}, "v":5.1, "access_token":{token}}})
+        .then(response => {console.log("Очков на вк:" + response);
+          // *назначение переменных*
+          score2= response;
+        })
+        .catch(error => console.log(error));
+        };
+        
