89fdd04988ba5b0831ea551e84665b8d6954efa1
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-06-19 15:50:23 +0300

    Add files via upload

--- aW1/App.js
+++ aW1/App.js
@@ -18,7 +18,7 @@ function favor1(){
 bridge.send("VKWebAppAddToFavorites");
 }
 
-function myadd1(){
+function myAdd1(){
   bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 .then(data => console.log(data.result))
 .catch(error => console.log(error));
