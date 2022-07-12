e9e4ea0cc1c09d6dbf9d1da0fa2d7cf99eb3303e
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-07-12 10:38:20 +0300

    Update dataN1_10.js

--- SM/circle/js/dataN1_10.js
+++ SM/circle/js/dataN1_10.js
@@ -5,121 +5,121 @@ const DATA = {
             "character": "僚",
             "onyomi": "リョウ",
             "kunyomi": "",
-            "meaning": "colleague, official, companion"
+            "meaning": "Коллега"
         }, {
             "category": "jlptn1",
             "character": "吉",
-            "onyomi": "キチ キツ ヨ",
+            "onyomi": "キチ キツ",
             "kunyomi": "",
-            "meaning": "good luck, joy, congratulations"
+            "meaning": "Удача, счастье"
         }, {
             "category": "jlptn1",
             "character": "盛",
-            "onyomi": "セイ ジョウ サカ.",
+            "onyomi": "セイ ジョウ",
             "kunyomi": "も.る さか.る",
-            "meaning": "boom, prosper, copulate"
+            "meaning": "Процветать"
         }, {
             "category": "jlptn1",
             "character": "皇",
             "onyomi": "コウ オ",
             "kunyomi": "",
-            "meaning": "emperor"
+            "meaning": "Император"
         }, {
             "category": "jlptn1",
             "character": "臨",
             "onyomi": "リン ノゾ.",
-            "kunyomi": "",
-            "meaning": "look to, face, meet, confront, attend, call on"
+            "kunyomi": "のぞ.む",
+            "meaning": "Присутствовать"
         }, {
             "category": "jlptn1",
             "character": "踏",
             "onyomi": "トウ",
             "kunyomi": "ふ.む ふ.まえる",
-            "meaning": "step, trample, carry through, appraise, evade payment"
+            "meaning": "Наступать"
         }, {
             "category": "jlptn1",
             "character": "壊",
             "onyomi": "カイ エ",
             "kunyomi": "こわ.す こわ.れる やぶ.る",
-            "meaning": "demolition, break, destroy"
+            "meaning": "Разрушать"
         }, {
             "category": "jlptn1",
             "character": "債",
             "onyomi": "サイ",
             "kunyomi": "",
-            "meaning": "bond, loan, debt"
+            "meaning": "Заём"
         }, {
             "category": "jlptn1",
             "character": "興",
-            "onyomi": "コウ キョウ オコ.",
+            "onyomi": "コウ キョウ",
             "kunyomi": "おこ.る",
-            "meaning": "entertain, revive, retrieve, interest, pleasure"
+            "meaning": "Развлечение"
         }, {
             "category": "jlptn1",
             "character": "源",
-            "onyomi": "ゲン ミナモ",
-            "kunyomi": "",
-            "meaning": "source, origin"
+            "onyomi": "ゲン",
+            "kunyomi": "みなもと",
+            "meaning": "Исток"
         }, {
             "category": "jlptn1",
             "character": "儀",
-            "onyomi": "",
-            "kunyomi": "",
-            "meaning": "ceremony, rule, affair, case, a matter"
+            "onyomi": "ギ",
+            "kunyomi": "ギ",
+            "meaning": "Церемония"
         }, {
             "category": "jlptn1",
             "character": "創",
-            "onyomi": "ソウ ショウ ケズ.シケ",
+            "onyomi": "ソウ",
             "kunyomi": "つく.る はじ.める きず",
-            "meaning": "genesis, wound, injury, hurt, start, originate"
+            "meaning": "Творческий, создавать"
         }, {
             "category": "jlptn1",
             "character": "障",
             "onyomi": "ショウ",
             "kunyomi": "さわ.る",
-            "meaning": "hinder, hurt, harm"
+            "meaning": "Препятствие, мешать"
         }, {
             "category": "jlptn1",
             "character": "継",
             "onyomi": "ケイ",
             "kunyomi": "つ.ぐ まま-",
-            "meaning": "inherit, succeed, patch, graft (tree)"
+            "meaning": "Следовать"
         }, {
             "category": "jlptn1",
             "character": "筋",
             "onyomi": "キン",
             "kunyomi": "すじ",
-            "meaning": "muscle, sinew, tendon, fiber, plot, plan, descent"
+            "meaning": "Мышца, мускул"
         }, {
             "category": "jlptn1",
             "character": "闘",
-            "onyomi": "トウ アラソ.",
+            "onyomi": "トウ",
             "kunyomi": "たたか.う",
-            "meaning": "fight, war"
+            "meaning": "Бороться"
         }, {
             "category": "jlptn1",
             "character": "葬",
-            "onyomi": "ソウ ホウム.",
-            "kunyomi": "",
-            "meaning": "interment, bury, shelve"
+            "onyomi": "ソウ",
+            "kunyomi": "ほうむ.る",
+            "meaning": "Похороны"
         }, {
             "category": "jlptn1",
             "character": "避",
             "onyomi": "ヒ",
             "kunyomi": "さ.ける よ.ける",
-            "meaning": "evade, avoid, avert, ward off, shirk, shun"
+            "meaning": "Избегать"
         }, {
             "category": "jlptn1",
             "character": "司",
-            "onyomi": "シ ツカサド.",
-            "kunyomi": "",
-            "meaning": "director, official, govt office, rule, administer"
+            "onyomi": "シ.",
+            "kunyomi": "つかさど.る",
+            "meaning": "Управляющий"
         }, {
             "category": "jlptn1",
             "character": "康",
-            "onyomi": "コ",
+            "onyomi": "コウ",
             "kunyomi": "",
-            "meaning": "ease, peace"
+            "meaning": "Здоровье"
         }    ],
     vocabulary: [
 
