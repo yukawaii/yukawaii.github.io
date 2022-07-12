6d9c5a8cd5490f38b0533fe425ff7c52a15dbd3c
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-07-12 10:04:03 +0300

    Update dataN1_1.js

--- SM/circle/js/dataN1_1.js
+++ SM/circle/js/dataN1_1.js
@@ -5,121 +5,121 @@ const DATA = {
             "character": "省",
             "onyomi": "セイ ショウ ハブ.",
             "kunyomi": "かえり.みる",
-            "meaning": "focus, government ministry, conserve"
+            "meaning": "Министерство, рефлексия"
         }, {
             "category": "jlptn2",
             "character": "旧",
             "onyomi": "キュウ",
             "kunyomi": "ふる.い もと",
-            "meaning": "old times, old things, old friend, former, ex-"
+            "meaning": "Прежний, экс-"
         }, {
             "category": "jlptn2",
             "character": "橋",
             "onyomi": "キョウ ハ",
-            "kunyomi": "",
-            "meaning": "bridge"
+            "kunyomi": "はし",
+            "meaning": "Мост"
         }, {
             "category": "jlptn2",
             "character": "岸",
             "onyomi": "ガン キ",
-            "kunyomi": "",
+            "kunyomi": "きし",
             "meaning": "beach"
         }, {
             "category": "jlptn2",
             "character": "周",
             "onyomi": "シュウ マワ.",
-            "kunyomi": "",
-            "meaning": "circumference, circuit, lap"
+            "kunyomi": "まわり",
+            "meaning": "Круг, кайма"
         }, {
             "category": "jlptn2",
             "character": "材",
-            "onyomi": "ザ",
+            "onyomi": "ザイ",
             "kunyomi": "",
-            "meaning": "lumber, log, timber, wood, talent"
+            "meaning": "Материал, способность"
         }, {
             "category": "jlptn2",
             "character": "戸",
             "onyomi": "コ",
-            "kunyomi": "",
-            "meaning": "door"
+            "kunyomi": "と",
+            "meaning": "Дверь"
         }, {
             "category": "jlptn2",
             "character": "央",
-            "onyomi": "オ",
+            "onyomi": "オウ",
             "kunyomi": "",
-            "meaning": "center, middle"
+            "meaning": "Центр"
         }, {
             "category": "jlptn2",
             "character": "券",
             "onyomi": "ケン",
             "kunyomi": "",
-            "meaning": "ticket"
+            "meaning": "Билет"
         }, {
             "category": "jlptn2",
             "character": "編",
             "onyomi": "ヘン",
             "kunyomi": "あ.む -あ.み",
-            "meaning": "compilation, knit, plait, braid, twist, editing, completed poem, part of a book"
+            "meaning": "Вязать, плести"
         }, {
             "category": "jlptn2",
             "character": "捜",
-            "onyomi": "ソウ シュ シュウ",
+            "onyomi": "ソウ",
             "kunyomi": "さが.す",
-            "meaning": "search, look for, locate"
+            "meaning": "Искать"
         }, {
             "category": "jlptn2",
             "character": "竹",
-            "onyomi": "チク タ",
-            "kunyomi": "",
-            "meaning": "bamboo"
+            "onyomi": "チク",
+            "kunyomi": "たけ",
+            "meaning": "Бамбук"
         }, {
             "category": "jlptn2",
             "character": "超",
             "onyomi": "チョウ コ.",
             "kunyomi": "こ.える",
-            "meaning": "transcend, super-, ultra-"
+            "meaning": "Сверх-, превосходить"
         }, {
             "category": "jlptn2",
             "character": "並",
             "onyomi": "ヘイ ホウ ナラ.ビ",
             "kunyomi": "な.み なら.べる なら.ぶ",
-            "meaning": "row, and, besides, as well as, line up, rank with, rival, equal"
+            "meaning": "Выстраивать в ряд"
         }, {
             "category": "jlptn2",
             "character": "療",
             "onyomi": "リョウ",
             "kunyomi": "",
-            "meaning": "heal, cure"
+            "meaning": "Лечение"
         }, {
             "category": "jlptn2",
             "character": "採",
             "onyomi": "サイ",
             "kunyomi": "と.る",
-            "meaning": "pick, take, fetch, take up"
+            "meaning": "Добывать"
         }, {
             "category": "jlptn2",
             "character": "森",
             "onyomi": "シン モ",
-            "kunyomi": "",
-            "meaning": "forest, woods"
+            "kunyomi": "もり",
+            "meaning": "Лес"
         }, {
             "category": "jlptn2",
             "character": "競",
-            "onyomi": "キョウ ケイ セ.",
+            "onyomi": "キョウ ケイ",
             "kunyomi": "きそ.う",
-            "meaning": "emulate, compete with, bid, sell at auction, bout, contest, race"
+            "meaning": "Состязаться"
         }, {
             "category": "jlptn2",
             "character": "介",
-            "onyomi": "カ",
-            "kunyomi": "",
-            "meaning": "jammed in, shellfish, mediate, concern oneself with"
+            "onyomi": "カイ",
+            "kunyomi": "かい.して",
+            "meaning": "Посредничать"
         }, {
             "category": "jlptn2",
             "character": "根",
             "onyomi": "コン",
-            "kunyomi": "ね -ね",
-            "meaning": "root, radical, head (pimple)"
+            "kunyomi": "ね",
+            "meaning": "Корень, основание"
         }
     ],
     vocabulary: [
