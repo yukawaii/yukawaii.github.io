6cbd27b42e727811b339ade1f2dd365675cdfe8e
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-07-16 16:35:15 +0300

    Update script.js

--- SM/black/js/script.js
+++ SM/black/js/script.js
@@ -21,7 +21,7 @@ button1.addEventListener('click', () => {
 });
 const button2 = document.querySelector('#button2');
 
-button1.addEventListener('click', () => {
+button2.addEventListener('click', () => {
 window.location.href = './i.html';
 });
 
