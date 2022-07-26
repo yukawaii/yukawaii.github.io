e58cd82b72b6e263c7f82ea9073b5f8eec167183
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-07-26 12:53:47 +0300

    Update dataN1_15.js

--- SM/circle/js/dataN1_15.js
+++ SM/circle/js/dataN1_15.js
@@ -5,61 +5,61 @@ const DATA = {
             "character": "焦",
             "onyomi": "ショウ",
             "kunyomi": "こ.げる こ.がす こ.がれる あせ.る",
-            "meaning": "char, hurry, impatient, irritate, burn, scorch, singe"
+            "meaning": "Подгорать, спешить, обжигать"
         }, {
             "category": "jlptn1",
             "character": "奪",
             "onyomi": "ダツ",
             "kunyomi": "うば.う",
-            "meaning": "rob, take by force, snatch away, dispossess, plunder, usurp"
+            "meaning": "Отнимать, захватывать, лишать"
         }, {
             "category": "jlptn1",
             "character": "災",
             "onyomi": "サイ",
             "kunyomi": "わざわ.い",
-            "meaning": "disaster, calamity, woe, curse, evil"
+            "meaning": "Бедствие, катастрофа"
         }, {
             "category": "jlptn1",
             "character": "浦",
             "onyomi": "ホ",
             "kunyomi": "うら",
-            "meaning": "bay, creek, inlet, gulf, beach, seacoast"
+            "meaning": "Бухта, залив"
         }, {
             "category": "jlptn1",
             "character": "析",
             "onyomi": "セキ",
-            "kunyomi": "",
-            "meaning": "chop, divide, tear, analyze"
+            "kunyomi": "セキ",
+            "meaning": "Анализ"
         }, {
             "category": "jlptn1",
             "character": "譲",
-            "onyomi": "ジョウ ユズ.",
-            "kunyomi": "",
-            "meaning": "defer, turnover, transfer, convey"
+            "onyomi": "ジョウ",
+            "kunyomi": "ゆず.る",
+            "meaning": "Отдавать,"
         }, {
             "category": "jlptn1",
             "character": "称",
-            "onyomi": "ショウ ホメ.",
-            "kunyomi": "たた.える とな.える あ.げる かな.う はか.り はか.る",
-            "meaning": "appellation, praise, admire, name, title, fame"
+            "onyomi": "ショウ",
+            "kunyomi": "たた.える とな.える",
+            "meaning": "Название, восхвалять, называть"
         }, {
             "category": "jlptn1",
             "character": "納",
-            "onyomi": "ノウ ナッ ナ ナン トウ オサ.マ",
-            "kunyomi": "おさ.める -おさ.める",
-            "meaning": "settlement, obtain, reap, pay, supply, store"
+            "onyomi": "ノウ ナッ",
+            "kunyomi": "おさ.める",
+            "meaning": "Уплачивать, снабжать"
         }, {
             "category": "jlptn1",
             "character": "樹",
             "onyomi": "ジュ",
-            "kunyomi": "",
-            "meaning": "timber trees, wood"
+            "kunyomi": "き",
+            "meaning": "Дерево"
         }, {
             "category": "jlptn1",
             "character": "挑",
             "onyomi": "チョウ",
             "kunyomi": "いど.む",
-            "meaning": "challenge, contend for, make love to"
+            "meaning": "Провоцировать, флиртовать"
         }, {
             "category": "jlptn1",
             "character": "誘",
