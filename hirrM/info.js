9227ca8a759bd0cd24f21067d93407c8df580ab9
yukawaii <psatotlasi@gmail.com>
2022-08-20 16:36:34 +0300

    1

--- hirrM/info.js
+++ hirrM/info.js
@@ -14,22 +14,7 @@ console.log("id^ "+ id);
 .catch(error => console.log(error));
   }
   getid();
-
-     //первичная отправка очков в вк, проверка на 0
-    function sendscore0(){
-        score0=1;
-        vkBridge.send("VKWebAppCallAPIMethod", {"method": "secure.addappEvent", "request_id": "32test", "params":
-     {"user_id":id,
-      "activity_id":2,
-       "value":score0, 
-       "v": "5.1", 
-       "access_token":"2612c80d2612c80d2612c80d77266e5ead226122612c80d446f8f02f2b5426621bfea1f"}})
-    .then(data => {console.log("Ответ на первичное добавление очков:" + data);
-    })
-    .catch(error => console.log(error)); }
-    sendscore0();
-
-function gettoken(){
+  function gettoken(){
     vkBridge.send("VKWebAppGetAuthToken", { 
             "app_id": 8165024, 
             "scope": "friends,status"
@@ -41,7 +26,20 @@ function gettoken(){
     })
     .catch(error => console.log(error)); }
     
-    gettoken();    
+    gettoken();  
+     //первичная отправка очков в вк, проверка на 0
+    function sendscore0(){
+        score0=1;
+        vkBridge.send("VKWebAppCallAPIMethod", {"method": "secure.addappEvent", "request_id": "32test", "params":
+     {"user_id":id,
+      "activity_id":2,
+       "value":score0, 
+       "v": "5.1", 
+       "access_token":token}})
+    .then(data => {console.log("Ответ на первичное добавление очков:" + data);
+    })
+    .catch(error => console.log(error)); }
+    sendscore0();
 
  function getsc(){
     vkBridge.send("VKWebAppCallAPIMethod", {"method": "apps.getScore", "request_id": "32test", "params":
