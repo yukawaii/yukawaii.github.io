c1cdf834fc12fe361dec3b1a9e1fb1ebfc8f087c
yukawaii <psatotlasi@gmail.com>
2022-08-21 11:07:42 +0300

    1

--- kaM/App.js
+++ kaM/App.js
@@ -1,29 +1,23 @@
-//инициализация
-const bridge = vkBridge.default;
-bridge.subscribe((e) => console.log("vkBridge event", e));
-bridge.send("VKWebAppInit", {});
-
 //поделиться
-function share1(){
-bridge.send("VKWebAppShare", {"text": "Интересные игры!", "link": "https://vk.com/app8165904"});
-}
 function share2(){
-  bridge.send("VKWebAppShowWallPostBox", {
-    "message": "Игры для быстрого запоминания катаканы!",
-    "attachments": "https://vk.com/app8165904"
+  vkBridge.send("VKWebAppShowWallPostBox", {
+    "message": "Игры для быстрого запоминания хираганы!",
+    "attachments": "https://vk.com/app8165024"
   });
 }
 
 function favor1(){
-bridge.send("VKWebAppAddToFavorites");
+  vkBridge.send("VKWebAppAddToFavorites");
 }
-
 function myadd1(){
-  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
-  bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+  vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 .then(data => console.log(data.result))
 .catch(error => console.log(error));
 }
 function joingroup(){
-  bridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+  vkBridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+}
+//пригласить друзей в игру
+function infr(){
+  vkBridge.send("VKWebAppShowInviteBox", {})
 }
