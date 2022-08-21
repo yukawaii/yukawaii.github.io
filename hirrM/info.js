7d9a052048e1f78868b44b94ef7cc98bbf91b21a
yukawaii <psatotlasi@gmail.com>
2022-08-21 09:20:19 +0300

    1

--- hirrM/info.js
+++ hirrM/info.js
@@ -44,6 +44,7 @@ setTimeout(function (){console.log("id^ "+ id);}, 3000);
     .then(data => {console.log("Ответ на первичное добавление очков:" + data);
     })
     .catch(error => console.log(error)); }, 3000);}
+    sendscore0();
 
  function getsc(){
   getid();
@@ -52,7 +53,7 @@ setTimeout(function (){console.log("id^ "+ id);}, 3000);
        {"user_id":id,
          "v": "5.131", 
          "access_token":"2612c80d2612c80d2612c80d77266e5ead226122612c80d446f8f02f2b5426621bfea1f"}})
-      .then(data => {console.log(data); score=data.response; console.log("getsc"+score); 
+      .then(data => {console.log(data); score=data.response; console.log("getsc=  "+score); 
       })
       .catch(error => console.log(error)); }, 2000);}
 getsc();
