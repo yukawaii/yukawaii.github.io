5649e1fbac2f4b5917153ac8932f24e2f5668fd5
yukawaii <psatotlasi@gmail.com>
2022-08-21 15:33:26 +0300

    1

--- Xi/info.js
+++ Xi/info.js
@@ -1,3 +1,11 @@
+import bridge from '@vkontakte/vk-bridge';
+
+// Sends event to client
+bridge.send('VKWebAppInit');
+
+// Subscribes to event, sended by client
+bridge.subscribe(e => console.log(e));
+
 var score,id,token, name1;
 
 function getid(){
