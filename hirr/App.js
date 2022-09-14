59be54beed9161324fff08e111db61235b1ab6ae
yukawaii <104377892+yukawaii@users.noreply.github.com>
2022-09-14 09:59:37 +0300

    Update App.js

--- hirr/App.js
+++ hirr/App.js
@@ -5,29 +5,29 @@ bridge.send("VKWebAppInit", {});
 
 //поделиться
 function share1(){
-bridge.send("VKWebAppShare", {"text": "Интересные игры!", "link": "vk.com/app8156273"});
+vkBridge.send("VKWebAppShare", {"text": "Интересные игры!", "link": "vk.com/app8156273"});
 }
 function share2(){
-  bridge.send("VKWebAppShowWallPostBox", {
+  vkBridge.send("VKWebAppShowWallPostBox", {
     "message": "Игры для быстрого запоминания хираганы!",
     "attachments": "https://vk.com/app8165024"
   });
 }
 
 function favor1(){
-bridge.send("VKWebAppAddToFavorites");
+vkBridge.send("VKWebAppAddToFavorites");
 }
 
 
  function myadd1(){
-  bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
-  bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+ 
+  vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
 .then(data => console.log(data.result))
 .catch(error => console.log(error));
 }
 
 function joingroup(){
-  bridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+vkBridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
 }
 
 /*
