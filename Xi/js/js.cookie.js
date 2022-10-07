bf995d8cabefa0b94f93622fbeb1c1343181a199
yukawaii <psatotlasi@gmail.com>
2022-10-07 19:29:29 +0300

    1

--- Xi/js/js.cookie.js
+++ Xi/js/js.cookie.js
@@ -1,10 +1,4 @@
-/*!
- * JavaScript Cookie v2.0.0-pre
- * https://github.com/js-cookie/js-cookie
- *
- * Copyright 2006, 2015 Klaus Hartl
- * Released under the MIT license
- */
+
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		define(factory);
