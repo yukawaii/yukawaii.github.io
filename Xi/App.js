6c4bcf32d6d8dd3bc553d68b75317cd2beab4360
yukawaii <104377892+yukawaii@users.noreply.github.com>
2024-05-21 22:50:19 +0300

    Update App.js

--- Xi/App.js
+++ Xi/App.js
@@ -14,9 +14,9 @@ function myadd1(){
 .then(data => console.log(data.result))
 .catch(error => console.log(error));
 }
-function joingroup(){
-  vkBridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
-}
+// function joingroup(){
+//   vkBridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+// }
 //пригласить друзей в игру
 function infr(){
   vkBridge.send("VKWebAppShowInviteBox", {})
