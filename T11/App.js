99443e7936df63630b1c07f097d817bfe33134b0
yukawaii <104377892+yukawaii@users.noreply.github.com>
2024-05-21 22:51:14 +0300

    Update App.js

--- T11/App.js
+++ T11/App.js
@@ -28,9 +28,9 @@ function myadd2(){
 .catch(error => console.log(error));
 }
 
-function joingroup(){
-  bridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
-}
+// function joingroup(){
+//   bridge.send("VKWebAppJoinGroup", {"group_id": 213417231});
+// }
 //     <button class = "iksweb" onclick="pauseGame(); myadd2()">Пауза</button><br />*/
 
 //Top-table
