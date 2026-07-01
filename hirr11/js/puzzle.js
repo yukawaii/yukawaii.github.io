0b0a989d5577751e61e30c8f10b363499c6dce5d
YulechkaPlay <104377892+yukawaii@users.noreply.github.com>
2026-07-01 21:04:53 +0300

    обновл

--- hirr11/js/puzzle.js
+++ hirr11/js/puzzle.js
@@ -204,11 +204,10 @@ renderWithImage(img, grid) {
             const srcRow = Math.floor(tileIndex / gridSize);
             const srcCol = tileIndex % gridSize;
             
-            // backgroundSize: размер всего изображения в пикселях
-           tile.style.backgroundImage = `url(${this.imageSrc})`;
-            tile.style.backgroundSize = fullSize + 'px ' + fullSize + 'px !important';
-            tile.style.backgroundPosition = `-${srcCol * size}px -${srcRow * size}px !important`;
-            tile.style.backgroundRepeat = 'no-repeat !important';
+            // обновление: размер всего изображения в пикселях
+        tile.style.background = `url(${this.imageSrc}) no-repeat ${-srcCol * size}px ${-srcRow * size}px / ${fullSize}px ${fullSize}px !important`;
+
+
             tile.style.cursor = 'pointer';
             tile.dataset.index = tileIndex;
             
